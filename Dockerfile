0fbbec1ad1c3b65dd61b9ef752886ec1a02dfe6d
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-06-25 14:50:37 +0200

    Add filebeat

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,16 @@ RUN ["luarocks", "install", "lua-resty-http"]
 RUN ["luarocks", "install", "lua-resty-jwt"]
 RUN ["luarocks", "install", "lua-resty-openidc"]
 
+#FILEBEAT
+COPY --from=osirixfoundation/kheops-beat:latest /install/deb/filebeat-amd64.deb .
+RUN dpkg -i filebeat-amd64.deb && \
+    rm filebeat-amd64.deb
+
+COPY filebeat.yml /etc/filebeat/filebeat.yml
+RUN chmod go-w /etc/filebeat/filebeat.yml
+
+RUN rm /usr/local/openresty/nginx/logs/access.log /usr/local/openresty/nginx/logs/error.log && \
+    rm /etc/nginx/conf.d/default.conf
+
+COPY script.sh /script.sh
+ENTRYPOINT ["/script.sh"]
---
+++ script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#######################################################################################
+#ELASTIC SEARCH
+
+missing_env_var_secret=false
+
+if ! [ -z "$MEDIRAD_NGINX_ENABLE_ELASTIC" ]; then
+    if [ "$MEDIRAD_NGINX_ENABLE_ELASTIC" = true ]; then
+
+        echo "Start init filebeat"
+        missing_env_var_secret=false
+
+        if [ -z $MEDIRAD_NGINX_LOGSTASH_URL ]; then
+          echo "Missing MEDIRAD_NGINX_LOGSTASH_URL environment variable"
+          missing_env_var_secret=true
+        else
+           echo -e "environment variable MEDIRAD_NGINX_LOGSTASH_URL OK"
+           sed -i "s|\${logstash_url}|$MEDIRAD_NGINX_LOGSTASH_URL|" /etc/filebeat/filebeat.yml
+        fi
+
+        if [[ -z $MEDIRAD_NGINX_ELASTIC_INSTANCE ]]; then
+           echo "Missing MEDIRAD_NGINX_ELASTIC_INSTANCE environment variable"
+           missing_env_var_secret=true
+        else
+            echo -e "environment variable MEDIRAD_NGINX_ELASTIC_INSTANCE \e[92mOK\e[0m"
+            sed -i "s|\${instance}|$MEDIRAD_NGINX_ELASTIC_INSTANCE|" /etc/filebeat/filebeat.yml
+        fi
+
+
+        #if missing env var or secret => exit
+        if [ $missing_env_var_secret = true ]; then
+          exit 1
+        else
+           echo "all elastic secrets and all env var OK"
+        fi
+
+        filebeat modules disable system
+        service filebeat restart
+
+        echo "Ending setup FILEBEAT"
+    fi
+else
+    echo "[INFO] : Missing MEDIRAD_NGINX_ENABLE_ELASTIC environment variable. Elastic is not enable."
+fi
+
+#######################################################################################
+
+./usr/local/openresty/bin/openresty -g 'daemon off;'
